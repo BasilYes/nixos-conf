a2538f218ca5a723246739e6ef042fa33e8c3739
BasilYes <basilyevs@gmail.com>
2024-05-21 14:01:43 +0300

    flake init

--- basilyes.nix
+++ basilyes.nix
@@ -1,55 +1,6 @@
 { config, pkgs, ... }:
 
-let
-  home-manager = builtins.fetchTarball "https://github.com/nix-community/home-manager/archive/master.tar.gz";
-in
 {
-  imports = [
-    (import "${home-manager}/nixos")
-  ];
-
-  home-manager.users.basilyes = {
-    home.packages = with pkgs; [
-      neofetch
-    ];
-  
-    services.easyeffects.enable = true;
-
-    programs.home-manager.enable = true;
-    programs.bash = {
-      enable = true;
-      bashrcExtra = ''
-        eval "$(atuin init bash)"
-        export EDITOR=vim'';
-    };
-    programs.git = {
-      enable = true;
-      userName = "BasilYes";
-      userEmail = "basilyes@gmail.com";
-      aliases = {
-        la = "!git config -l | grep alias | cut -c 7-";
-        cm = "commit -m";
-        ca = "commit --amend";
-        cam = "commit --amend -m";
-        st = "status -sb";
-        b = "!git for-each-ref --sort='-authordate' --format='%(authordate)%09%(objectname:short)%09%(refname)' refs/heads | sed -e 's-refs/heads/--'";
-        lg1 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(auto)%d%C(reset)' --all";
-        lg2 = "log --graph --abbrev-commit --decorate --format=format:'%C(bold blue)%h%C(reset) - %C(bold cyan)%aD%C(reset) %C(bold green)(%ar)%C(reset)%C(auto)%d%C(reset)%n''          %C(white)%s%C(reset) %C(dim white)- %an%C(reset)'";
-        lg = "lg1";
-      };
-      extraConfig = {
-        init = { defaultBranch = "main"; };
-        safe = { directory = "/etc/nixos"; };
-      };
-    };
-    
-    home.stateVersion = "23.11";
-  };
-  
-  home-manager.useGlobalPkgs = true;
-  home-manager.useUserPackages = true;
-  
-
   environment.systemPackages = with pkgs; [
     libnotify
   ];
---
+++ flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "A simple NixOS flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+
+    home-manager = {
+      url = "github:nix-community/home-manager/master";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+  };
+
+  outputs = inputs@{ nixpkgs, home-manager, ... }: {
+    nixosConfigurations.basilyes-desktop = nixpkgs.lib.nixosSystem {
+      system = "x86_64-linux";
+      modules = [
+        ./configuration.nix
+        home-manager.nixosModules.home-manager
+        {
+          home-manager.useGlobalPkgs = true;
+          home-manager.useUserPackages = true;
+
+          home-manager.users.basilyes = import ./home.nix;
+        }
+      ];
+    };
+  };
+}
