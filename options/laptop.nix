c86d529e803d06922784941e743755c990b3253f
BasilYes <basilyevs@gmail.com>
2024-06-24 00:26:30 +0300

    laptop config

---
+++ options/laptop.nix
@@ -0,0 +1,16 @@
+{
+	hostName = "basilyes-laptop";
+	userName = "basilyes";
+	hardwareFile = "laptop.nix";
+	nvidia = false;
+	gaming = true;
+	develop = true;
+	android = true;
+	optionals = false;
+	vm = true;
+	hyprland = true;
+	gnome = true;
+	kde = false;
+	# gdm or sddm
+	displayManager = "gdm";
+}
