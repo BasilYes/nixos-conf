d87a35d99dca43be404ff22c855cb9914008d2ac
BasilYes <basilyevs@gmail.com>
2024-06-26 21:57:58 +0300

    rework workspaces

--- .config/hypr/scripts/start.sh
+++ .config/hypr/scripts/start.sh
@@ -13,4 +13,9 @@ otd-daemon &
 bash ~/.config/waybar/launch.sh &
 rm -f /tmp/hypr/switch_*_temp &
 
-swaync
+swaync &
+
+hyprctl --batch "dispatch focusmonitor 1; dispatch workspace 12;" &&
+sleep 0.1 &&
+hyprctl dispatch "workspace 1" &&
+hyprctl --batch "dispatch focusmonitor 0; dispatch workspace 11;"
