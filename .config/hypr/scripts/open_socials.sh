3af72b25e4eab90bc34fa3ac329ed7a8bfb61fb6
BasilYes <basilyevs@gmail.com>
2024-07-01 18:20:48 +0300

    update socials

--- .config/hypr/scripts/open_socials.sh
+++ .config/hypr/scripts/open_socials.sh
@@ -1,14 +1,14 @@
 #!/usr/bin/env bash
 
 hyprctl --batch "
-	dispatch exec [group set active] telegram-desktop;
+	dispatch exec [group set always] telegram-desktop;
+	dispatch exec [group set always] evolution;
 "
-sleep 0.5
+sleep 2.0
 hyprctl --batch "
-	dispatch exec vivaldi --profile-directory=Default --app=https://app.element.io;
-	dispatch exec vivaldi --profile-directory=Default --app=https://discord.com/app;
-	dispatch exec vivaldi --profile-directory=Default --app=https://vk.com/im;
-	dispatch exec evolution;
+	dispatch exec [group set always] vivaldi --profile-directory=Default --app=https://app.element.io;
+	dispatch exec [group set always] vivaldi --profile-directory=Default --app=https://discord.com/app;
+	dispatch exec [group set always] vivaldi --profile-directory=Default --app=https://vk.com/im;
 "
 # sleep 0.5
 # hyprctl dispatch changegroupactive f;
