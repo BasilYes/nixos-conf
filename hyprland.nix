c252844e2bf9a18225d649a4561da24b491c0489
BasilYes <basilyevs@gmail.com>
2024-07-03 15:01:13 +0300

    add gst pluginst to see files info inside nautilus

--- hyprland.nix
+++ hyprland.nix
@@ -26,6 +26,14 @@
 	services.displayManager.defaultSession = "hyprland";
 	services.gnome.evolution-data-server.enable = true;
 
+	environment.sessionVariables.GST_PLUGIN_SYSTEM_PATH_1_0 = lib.makeSearchPathOutput "lib" "lib/gstreamer-1.0" [
+		pkgs.gst_all_1.gst-plugins-good
+		pkgs.gst_all_1.gst-plugins-bad
+		pkgs.gst_all_1.gst-plugins-ugly
+		pkgs.gst_all_1.gst-plugins-base
+		pkgs.gst_all_1.gst-plugins-rs
+	];
+
   environment.systemPackages = with pkgs; [
     gnome.nautilus
 		(waybar.overrideAttrs (oldAttrs: {
