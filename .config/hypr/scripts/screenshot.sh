9b200d3009be5fd57e938eac11d800a994c350b4
BasilYes <basilyevs@gmail.com>
2024-07-04 22:39:12 +0300

    update script

--- .config/hypr/scripts/screenshot.sh
+++ .config/hypr/scripts/screenshot.sh
@@ -16,40 +16,39 @@ if [[ $1 == r ]]; then
     mkdir -p $HOME/Pictures/Screenshots
     dirname=$HOME/Pictures/Screenshots/
 		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		hyprshot -m region -o $dirname -f $filename -- echo
+		hyprshot -m region -o $dirname -f $filename --freeze
 
 elif [[ $1 == ri ]]; then
     mkdir -p $HOME/Pictures/Screenshots
-    dirname=$HOME/Pictures/Screenshots/
-		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		filename=$(hyprshot -m region -o $dirname -f $filename -- echo)
-		satty --filename $filename --fullscreen --output-filename $filename --early-exit --save-after-copy
+		filename=$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
+		hyprshot -m region --freeze --clipboard-only --raw |
+		satty --filename - --output-filename $filename --initial-tool crop --copy-command wl-copy --early-exit --save-after-copy
+		# filename=$(hyprshot -m region -o $dirname -f $filename -- echo)
+		# satty --filename $filename --fullscreen --output-filename $filename --copy-command wl-copy --early-exit --save-after-copy
 
 elif [[ $1 == s ]]; then
     mkdir -p $HOME/Pictures/Screenshots
     dirname=$HOME/Pictures/Screenshots/
 		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		hyprshot -m output -m active -o $dirname -f $filename -- echo
+		hyprshot -m output -m active -o $dirname -f $filename --freeze
 
 elif [[ $1 == si ]]; then
     mkdir -p $HOME/Pictures/Screenshots
-    dirname=$HOME/Pictures/Screenshots/
-		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		filename=$(hyprshot -m output -m active -o $dirname -f $filename -- echo)
-		satty --filename $filename --output-filename $filename --initial-tool crop --early-exit --save-after-copy
+		filename=$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
+		hyprshot -m output -m active --freeze --clipboard-only --raw |
+		satty --filename - --output-filename $filename --initial-tool crop --copy-command wl-copy --early-exit --save-after-copy
 
 elif [[ $1 == w ]]; then
     mkdir -p $HOME/Pictures/Screenshots
     dirname=$HOME/Pictures/Screenshots/
 		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		hyprshot -m window -m active -o $dirname -f $filename -- echo
+		hyprshot -m window -m active -o $dirname -f $filename --freeze
 
 elif [[ $1 == wi ]]; then
     mkdir -p $HOME/Pictures/Screenshots
-    dirname=$HOME/Pictures/Screenshots/
-		filename=$(date +%Y-%m-%d_%H-%M-%S).png
-		filename=$(hyprshot -m window -m active -o $dirname -f $filename -- echo)
-		satty --filename $filename --output-filename $filename --initial-tool crop --early-exit --save-after-copy
+		filename=$HOME/Pictures/Screenshots/$(date +%Y-%m-%d_%H-%M-%S).png
+		hyprshot -m window -m active --freeze --clipboard-only --raw |
+		satty --filename - --output-filename $filename --initial-tool crop --copy-command wl-copy --early-exit --save-after-copy
 
 elif [[ $1 == p ]]; then
     color=$(hyprpicker -a)
