efe95a856a6b780751fef7e782b71acdf58dca86
BasilYes <basilyevs@gmail.com>
2024-07-13 00:07:21 +0300

    qt5 breeze theme

--- theming.nix
+++ theming.nix
@@ -4,11 +4,42 @@
 {
 	qt = {
 		enable = true;
-		platformTheme.name = "gtk"; 
-		style = {
-			name = "adwaita-dark";
-		};
+		# platformTheme.name = "gtk"; 
+		# style = {
+		# 	name = "gtk2";
+		# 	package = pkgs.libsForQt5.breeze-qt5;
+		# 	# package = pkgs.kdePackages.breeze;
+		# };
+
+		platformTheme.name = "qtct";
+    # style.name = "kvantum";
+
+		# # platformTheme.name = "gtk"; 
+		# style = {
+		# 	name = "adwaita-dark";
+		# };
 	};
+
+  xdg.configFile = {
+    qt5ct = {
+      target = "qt5ct/qt5ct.conf";
+      text = lib.generators.toINI { } {
+        Appearance = {
+          icon_theme = "breeze-dark";
+					style = "Breeze";
+        };
+      };
+    };
+    qt6ct = {
+      target = "qt6ct/qt6ct.conf";
+      text = lib.generators.toINI { } {
+        Appearance = {
+          icon_theme = "breeze-dark";
+					style = "Breeze";
+        };
+      };
+    };
+  };
 
 	gtk = {
 		enable = true;
@@ -28,9 +59,9 @@
 		gtk3.extraConfig = {
 			gtk-application-prefer-dark-theme = true;
 		};
-		gtk2.extraConfig = ''
-			gtk-application-prefer-dark-theme = 1
-		'';
+		# gtk2.extraConfig = ''
+		# 	gtk-application-prefer-dark-theme = 1
+		# '';
 	};
 
   dconf = {
@@ -40,12 +71,17 @@
 		};
   }; 
 
-  # home.sessionVariables = {
-  #   GTK_THEME = "Adwaita:dark";
-  # };
+  home.sessionVariables = {
+		QT_QPA_PLATFORMTHEME = "qt5ct";
+    # GTK_THEME = "Adwaita:dark";
+  };
 	
 	home.packages = with pkgs; [
 		gnome.gnome-themes-extra
+		# kdePackages.breeze
+		# kdePackages.breeze-icons
+		libsForQt5.breeze-qt5
+		libsForQt5.breeze-icons
 	# 	gnome.adwaita-icon-theme
 	#		adw-gtk3
 	#		adwaita-qt
