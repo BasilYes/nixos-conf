dcec9fd63d659c8a70cc9dae8a5039cf550ddabb
BasilYes <basilyevs@gmail.com>
2024-07-14 11:03:40 +0300

    update temperature widget

--- .config/hypr/scripts/temperature.sh
+++ .config/hypr/scripts/temperature.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-sensors -j | grep -P "temp._input" | tr -d " ," | awk 'BEGIN { RS=":"; } NR!=1 {print $1}' | sort -n | tail -1 | awk 'BEGIN { RS="."; } NR==1 {print $1}'
+max_temp=$(sensors -j | grep -P "temp._input" | tr -d " ," | awk 'BEGIN { RS=":"; } NR!=1 {print $1}' | sort -n | tail -1 | awk 'BEGIN { RS="."; } NR==1 {print $1}')
+sensors=$(sensors | tr '\n' '\r')
+echo { \"text\": \"$max_temp\", \"tooltip\": \"$sensors\" }
+# | jq --unbuffered --compact-output
