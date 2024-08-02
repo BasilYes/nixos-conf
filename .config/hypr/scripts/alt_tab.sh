5d3359078f13d1e4960d59ee59d4f3d9069388c5
BasilYes <basilyevs@gmail.com>
2024-08-02 15:26:12 +0300

    smart alt tab

---
+++ .config/hypr/scripts/alt_tab.sh
@@ -0,0 +1,18 @@
+#!/usr/bin/env bash
+
+window=$(hyprctl activewindow -j)
+group=$(echo $window | jq -cr '.grouped')
+
+if [[ $group == "[]" ]]; then
+    if [[ $1 == "prev" ]]; then
+        hyprctl dispatch cyclenext prev
+    else
+        hyprctl dispatch cyclenext
+    fi
+else
+    if [[ $1 == "prev" ]]; then
+        hyprctl dispatch changegroupactive b
+    else
+        hyprctl dispatch changegroupactive f
+    fi
+fi
