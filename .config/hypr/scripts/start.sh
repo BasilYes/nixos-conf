4666feddb2190508672ee5ae56b50431464567f6
BasilYes <basilyevs@gmail.com>
2024-08-09 12:17:39 +0300

    run power tunnel inside start scrips

--- .config/hypr/scripts/start.sh
+++ .config/hypr/scripts/start.sh
@@ -16,4 +16,8 @@ swaync &
 
 kitty &
 
+if [[ -f $HOME/Programs/PowerTunnel.jar ]]; then
+	hyprctl dispatch "exec [workspace special:pocket0 silent] java -jar ~/Programs/PowerTunnel.jar --start" &
+fi
+
 sleep 2 && bash ~/.config/hypr/scripts/monitors_setup.sh 1
