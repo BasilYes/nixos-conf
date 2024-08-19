cbb963a263557708dc4b2dc49e43a24691ff989e
BasilYes <basilyevs@gmail.com>
2024-08-19 17:01:23 +0300

    toggle waybar

--- .config/waybar/toggle.sh
+++ .config/waybar/toggle.sh
@@ -5,6 +5,5 @@ if [[ $1 == true || -z $1 ]] && [[ -z $pwaybar ]]; then
 	waybar &
 	blueman-applet
 elif [[ $1 == false || -z $1 ]] && [[ -n $pwaybar ]]; then
-	pkill waybar
-	pkill blueman &
+	bash ~/.config/hypr/scripts/switch.sh waybar "pkill -SIGUSR1 waybar" "pkill -SIGUSR2 waybar"
 fi
