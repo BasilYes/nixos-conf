e10ef569d167c29c6cd4da45ddf0fde3102dbc48
BasilYes <basilyevs@gmail.com>
2024-08-22 10:49:59 +0300

    add monitors setup

--- .config/hypr/scripts/monitors_setup.sh
+++ .config/hypr/scripts/monitors_setup.sh
@@ -10,6 +10,9 @@ if [ $system == "basilyes-desktop" ]; then
 	elif [[ $1 -eq 2 ]]; then
 		hyprctl keyword "monitor" "HDMI-A-1, 1920x1080@60, 0x0, 1"
 		hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, -1920x0, 1"
+	elif [[ $1 -eq 3 ]]; then
+		hyprctl keyword "monitor" "HDMI-A-1, disable"
+		hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, 0x0, 1"
 	fi
 elif [ $system == "basilyes-laptop" ]; then
 	hyprctl keyword "monitor" "eDP-1, 1920x1080@60, 0x0, 1"
