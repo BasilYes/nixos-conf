6abdaf480a6319b06c6bb5a9a0f4a1f8a38e7ac0
BasilYes <basilyevs@gmail.com>
2024-09-06 20:58:06 +0300

    delay socials

--- .config/hypr/scripts/open_socials.sh
+++ .config/hypr/scripts/open_socials.sh
@@ -6,9 +6,9 @@ hyprctl --batch "
 "
 sleep 2.0
 hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Discord"
-sleep 0.1
+sleep 0.5
 hyprctl	dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Element"
-sleep 0.1
+sleep 0.5
 hyprctl	dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh VK"
 # sleep 0.5
 # hyprctl dispatch changegroupactive f;
