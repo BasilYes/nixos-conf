5bc2328276889508b43bfa3a09f28a39e7a11d96
BasilYes <basilyevs@gmail.com>
2024-09-26 08:21:00 +0300

    format switch

--- .config/hypr/scripts/switch.sh
+++ .config/hypr/scripts/switch.sh
@@ -3,15 +3,15 @@
 file=/tmp/hypr/switch_${1}_temp
 
 if [[ -f "$file" ]]; then
-	switch=$(<"$file")
+    switch=$(<"$file")
 else
-	switch="false"
+    switch="false"
 fi
 
 if [[ "$switch" == "true" ]]; then
-	echo "false" > "$file"
-	hyprctl dispatch "exec ${3}"
+    echo "false" > "$file"
+    hyprctl dispatch "exec ${3}"
 else
-	echo "true" > "$file"
-	hyprctl dispatch "exec ${2}"
+    echo "true" > "$file"
+    hyprctl dispatch "exec ${2}"
 fi
