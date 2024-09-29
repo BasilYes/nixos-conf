39470b56748118c20e50e82003970c63efde77fd
BasilYes <basilyevs@gmail.com>
2024-09-29 17:01:07 +0300

    update superprod to 10.0.11

--- derivations/super-productivity.nix
+++ derivations/super-productivity.nix
@@ -2,11 +2,11 @@
 
 stdenv.mkDerivation rec {
   pname = "super-productivity";
-  version = "10.0.9";
+  version = "10.0.11";
 
   src = fetchurl {
     url = "https://github.com/johannesjo/super-productivity/releases/download/v${version}/superProductivity-${version}.AppImage";
-    hash = "sha256-Y8KLUjQd3fbo5PIDlyARcuTXMazydGzF8EH857h7C1k=";
+    hash = "sha256-sYHfzqP/Vla0DEReVjaPvo8fe1wNdZnNDhefqPrPFPE=";
     name = "${pname}-${version}.AppImage";
   };
 
