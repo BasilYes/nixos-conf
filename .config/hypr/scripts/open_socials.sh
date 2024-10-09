f07f2ec50d1153750dc0b173db5d7060d6cf43ba
BasilYes <basilyevs@gmail.com>
2024-10-09 15:35:06 +0300

    speedup socials

--- .config/hypr/scripts/open_socials.sh
+++ .config/hypr/scripts/open_socials.sh
@@ -1,14 +1,12 @@
 #!/usr/bin/env bash
 
+hyprctl dispatch "exec [group set always] vesktop"
 hyprctl --batch "
     dispatch exec [group set always] telegram-desktop;
     dispatch exec [group set always] thunderbird;
 "
-sleep 2.0
+sleep 4.0
 hyprctl dispatch "exec [group set always] flatpak run network.loki.Session"
-sleep 0.5
-# sleep 4.0
-hyprctl dispatch "exec [group set always] vesktop"
 # hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Discord"
 # sleep 0.5
 # hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Element"
