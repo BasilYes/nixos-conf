059da13f5c5c39935664e6d8b634b5bf21269ff9
BasilYes <basilyevs@gmail.com>
2024-10-11 18:39:09 +0300

    change socials script

--- .config/hypr/scripts/open_socials.sh
+++ .config/hypr/scripts/open_socials.sh
@@ -1,16 +1,13 @@
 #!/usr/bin/env bash
 
-hyprctl dispatch "exec [group set always] vesktop"
-hyprctl --batch "
-    dispatch exec [group set always] telegram-desktop;
-    dispatch exec [group set always] thunderbird;
-"
+hyprctl dispatch "exec [group set always;workspace name:socials silent] vesktop"
+hyprctl dispatch "exec [group set always;workspace name:socials silent] telegram-desktop"
+hyprctl dispatch "exec [group set always;workspace name:socials silent] thunderbird"
 sleep 4.0
-hyprctl dispatch "exec [group set always] flatpak run network.loki.Session"
+hyprctl dispatch "exec [group set always;workspace name:socials silent] ~/.config/hypr/scripts/open_PWA.sh Element"
+# sleep 0.5
 # hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Discord"
 # sleep 0.5
-# hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Element"
-# sleep 0.5
-# hyprctl    dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh VK"
+# hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh VK"
 # sleep 0.5
 # hyprctl dispatch changegroupactive f;
