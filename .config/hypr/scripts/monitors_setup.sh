d2614ebe910a52a0593368844f10be1e56c40886
BasilYes <basilyevs@gmail.com>
2024-10-15 08:23:55 +0300

    format monitors setup

--- .config/hypr/scripts/monitors_setup.sh
+++ .config/hypr/scripts/monitors_setup.sh
@@ -4,20 +4,20 @@
 system=$(hyprctl systeminfo | grep "Node name:" | sed "s@Node name: @@")
 
 if [ $system == "basilyes-desktop" ]; then
-	if [[ $1 -eq 1 ]]; then
-		hyprctl keyword "monitor" "HDMI-A-1, 1920x1080@60, 0x0, 1"
-		hyprctl keyword "monitor" "HDMI-A-3, disable"
-	elif [[ $1 -eq 2 ]]; then
-		hyprctl keyword "monitor" "HDMI-A-1, 1920x1080@60, 0x0, 1"
-		hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, -1920x0, 1"
-	elif [[ $1 -eq 3 ]]; then
-		hyprctl keyword "monitor" "HDMI-A-1, disable"
-		hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, 0x0, 1"
-	fi
+    if [[ $1 -eq 1 ]]; then
+        hyprctl keyword "monitor" "HDMI-A-1, 1920x1080@60, 0x0, 1"
+        hyprctl keyword "monitor" "HDMI-A-3, disable"
+    elif [[ $1 -eq 2 ]]; then
+        hyprctl keyword "monitor" "HDMI-A-1, 1920x1080@60, 0x0, 1"
+        hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, -1920x0, 1"
+    elif [[ $1 -eq 3 ]]; then
+        hyprctl keyword "monitor" "HDMI-A-1, disable"
+        hyprctl keyword "monitor" "HDMI-A-3, 1920x1080@60, 0x0, 1"
+    fi
 elif [ $system == "basilyes-laptop" ]; then
-	hyprctl keyword "monitor" "eDP-1, 1920x1080@60, 0x0, 1"
+    hyprctl keyword "monitor" "eDP-1, 1920x1080@60, 0x0, 1"
 fi
 
 if [[ -n $(pgrep "waybar") ]]; then
-	bash ~/.config/waybar/launch.sh
+    bash ~/.config/waybar/launch.sh
 fi
