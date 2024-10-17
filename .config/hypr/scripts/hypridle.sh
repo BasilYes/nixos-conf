ee4ae905f703917be7a43d90e30ff76ce2f1b62d
BasilYes <basilyevs@gmail.com>
2024-10-17 11:18:07 +0300

    update hyprlock

--- .config/hypr/scripts/hypridle.sh
+++ .config/hypr/scripts/hypridle.sh
@@ -2,12 +2,12 @@
 if $1; then
     if [[ -z $(hyprctl clients | grep "kitty-idle") ]]; then
         hyprctl dispatch "togglespecialworkspace idle"
-        bash ~/.config/hypr/scripts/keyboard_layout.sh 0
-        pidof hyprlock ||
-        hyprlock &&
-        hyprctl dispatch closewindow title:kitty-idle
+        # hyprctl dispatch closewindow title:kitty-idle
         # hyprctl dispatch submap idle
     fi
+    bash ~/.config/hypr/scripts/keyboard_layout.sh 0
+    pidof hyprlock ||
+    hyprlock
 else
     hyprctl dispatch closewindow title:kitty-idle
     # hyprctl dispatch submap reset
