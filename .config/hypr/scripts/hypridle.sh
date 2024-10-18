c39a450259d15f7b8bfc2185ce63a00dcf89c3e6
BasilYes <basilyevs@gmail.com>
2024-10-18 16:07:57 +0300

    fix suspend

--- .config/hypr/scripts/hypridle.sh
+++ .config/hypr/scripts/hypridle.sh
@@ -7,8 +7,8 @@ if $1; then
     fi
     bash ~/.config/hypr/scripts/keyboard_layout.sh 0
     pidof hyprlock ||
-    hyprlock &&
-    hyprctl dispatch closewindow title:kitty-idle
+    (hyprlock &&
+    hyprctl dispatch closewindow title:kitty-idle)
 else
     hyprctl dispatch closewindow title:kitty-idle
     # hyprctl dispatch submap reset
