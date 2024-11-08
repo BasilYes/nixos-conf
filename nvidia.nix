94e2e109bff5f0b73a1d61a465ddbdcd335262bc
BasilYes <basilyevs@gmail.com>
2024-11-08 09:28:03 +0300

    add egl wayland

--- nvidia.nix
+++ nvidia.nix
@@ -1,6 +1,6 @@
 { config, lib, pkgs, ... }:
 
-{  
+{
   hardware.graphics.enable = true;
 
   # Load nvidia driver for Xorg and Wayland
@@ -19,9 +19,9 @@
 
     # Use the NVidia open source kernel module (not to be confused with the
     # independent third-party "nouveau" open source driver).
-    # Support is limited to the Turing and later architectures. Full list of 
-    # supported GPUs is at: 
-    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus 
+    # Support is limited to the Turing and later architectures. Full list of
+    # supported GPUs is at:
+    # https://github.com/NVIDIA/open-gpu-kernel-modules#compatible-gpus
     # Only available from driver 515.43.04+
     # Currently alpha-quality/buggy, so false is currently the recommended setting.
     open = false;
@@ -32,18 +32,19 @@
 
     # Optionally, you may need to select the appropriate driver version for your specific GPU.
     package = config.boot.kernelPackages.nvidiaPackages.latest;
-		# package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
-		# 	version = "555.42.02";
-		# 	sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
-		# 	sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-		# 	openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
-		# 	settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA="; 
-		# 	persistencedSha256 = lib.fakeSha256;
-		# };
+    # package = config.boot.kernelPackages.nvidiaPackages.mkDriver {
+    #   version = "555.42.02";
+    #   sha256_64bit = "sha256-k7cI3ZDlKp4mT46jMkLaIrc2YUx1lh1wj/J4SVSHWyk=";
+    #   sha256_aarch64 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    #   openSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    #   settingsSha256 = "sha256-rtDxQjClJ+gyrCLvdZlT56YyHQ4sbaL+d5tL4L4VfkA=";
+    #   persistencedSha256 = lib.fakeSha256;
+    # };
   };
-  
+
   environment.systemPackages = with pkgs; [
-		nvidia-vaapi-driver
+    egl-wayland
+    nvidia-vaapi-driver
     nvidia-system-monitor-qt
     cudatoolkit
   ];
