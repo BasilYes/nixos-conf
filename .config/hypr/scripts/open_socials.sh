73ad100252ffc0729cbb747d3d761e269b9850c8
BasilYes <basilyevs@gmail.com>
2024-12-02 13:37:11 +0300

    rework socials script

--- .config/hypr/scripts/open_socials.sh
+++ .config/hypr/scripts/open_socials.sh
@@ -1,11 +1,12 @@
 #!/usr/bin/env bash
 
-hyprctl dispatch "exec [group set always;workspace name:socials silent] telegram-desktop"
 hyprctl dispatch "exec [group set always;workspace name:socials silent] thunderbird"
+sleep 1.0
+hyprctl dispatch "exec [group set always;workspace name:socials silent] telegram-desktop"
 sleep 2.5
 hyprctl dispatch "exec [group set always] ~/.config/hypr/scripts/open_PWA.sh Discord"
-sleep 0.5
-hyprctl dispatch "exec [group set always;workspace name:socials silent] ~/.config/hypr/scripts/open_PWA.sh Element"
+# sleep 0.5
+# hyprctl dispatch "exec [group set always;workspace name:socials silent] ~/.config/hypr/scripts/open_PWA.sh Element"
 sleep 2.0
 hyprctl --batch "dispatch workspace name:socials;dispatch lockactivegroup lock;"
 # sleep 0.5
