556371a3e3f722f570c4f287e124a36cff46608b
BasilYes <basilyevs@gmail.com>
2024-12-06 20:14:52 +0300

    simple suspend

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -3,6 +3,9 @@
 # lsusb | grep -i "logi"
 # grep c548 /sys/bus/usb/devices/*/idProduct
 
+systemctl suspend
+exit 0
+
 echo "For full suspend enter pass"
 sudo echo suspend
 bash ~/.nixos/.config/hypr/scripts/hypridle.sh &
