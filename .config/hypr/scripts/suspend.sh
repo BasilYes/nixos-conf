67219b52df41728e18e3acd923051deebadd7d90
BasilYes <basilyevs@gmail.com>
2024-12-22 23:58:22 +0300

    mouse / keyboard wakeup

--- .config/hypr/scripts/suspend.sh
+++ .config/hypr/scripts/suspend.sh
@@ -3,12 +3,12 @@
 # lsusb | grep -i "logi"
 # grep c548 /sys/bus/usb/devices/*/idProduct
 
-systemctl suspend
-exit 0
+# systemctl suspend
+# exit 0
 
 echo "For full suspend enter pass"
 sudo echo suspend
-bash ~/.nixos/.config/hypr/scripts/hypridle.sh &
+# bash ~/.nixos/.config/hypr/scripts/hypridle.sh &
 sleep 1.0 &&
 sudo echo disabled |
 sudo tee /sys/bus/usb/devices/*/power/wakeup && systemctl suspend
