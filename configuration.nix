055f427000ecdd39c164e778c5ad4ae6def6b145
BasilYes <basilyevs@gmail.com>
2025-01-11 16:17:21 +0300

    rework configuration

--- configuration.nix
+++ configuration.nix
@@ -29,98 +29,108 @@
 
   nix.settings.experimental-features = [ "nix-command" "flakes" ];
 
-
-  services.xserver.displayManager.${extraOptions.displayManager or "gdm"}.enable = true;
-
-  # Bootloader.
-  boot.loader.systemd-boot.enable = false;
-  #boot.loader.efi.canTouchEfiVariables = true;
-  boot.loader = {
-    efi = {
-      canTouchEfiVariables = true;
-      efiSysMountPoint = "/boot"; # ← use the same mount point here.
+  boot = {
+    # Bootloader.
+    loader = {
+      systemd-boot.enable = false;
+      efi = {
+        canTouchEfiVariables = true;
+        efiSysMountPoint = "/boot"; # ← use the same mount point here.
+      };
+      grub = {
+        enable = true;
+        efiSupport = true;
+        # efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
+        device = "nodev";
+        useOSProber = true;
+      };
     };
-    grub = {
-      enable = true;
-      efiSupport = true;
-      # efiInstallAsRemovable = true; # in case canTouchEfiVariables doesn't work for your system
-      device = "nodev";
-      useOSProber = true;
+    supportedFilesystems = [ "ntfs" ];
+    binfmt.registrations.appimage = {
+      wrapInterpreterInShell = false;
+      interpreter = "${pkgs.appimage-run}/bin/appimage-run";
+      recognitionType = "magic";
+      offset = 0;
+      mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
+      magicOrExtension = ''\x7fELF....AI\x02'';
     };
   };
 
-  boot.supportedFilesystems = [ "ntfs" ];
-
-  boot.binfmt.registrations.appimage = {
-    wrapInterpreterInShell = false;
-    interpreter = "${pkgs.appimage-run}/bin/appimage-run";
-    recognitionType = "magic";
-    offset = 0;
-    mask = ''\xff\xff\xff\xff\x00\x00\x00\x00\xff\xff\xff'';
-    magicOrExtension = ''\x7fELF....AI\x02'';
+  hardware = {
+    graphics.enable = true;
+    bluetooth.enable = true; # enables support for Bluetooth
+    bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
   };
 
-  hardware.graphics.enable = true;
-  hardware.bluetooth.enable = true; # enables support for Bluetooth
-  hardware.bluetooth.powerOnBoot = true; # powers up the default Bluetooth controller on boot
-
-  services.xserver.wacom.enable = true;
   # hardware.opentabletdriver = {
   #   enable = true;
   #   daemon.enable = true;
   # };
 
-  networking.hostName = extraOptions.hostName;
-
-  # Enable networking
-  networking.networkmanager.enable = true;
+  networking = {
+    hostName = extraOptions.hostName;
+    networkmanager.enable = true;
+    firewall = {
+      checkReversePath = false;
+      allowedTCPPorts = [ 42000 42001 7777 8060 ];
+      allowedUDPPorts = [ 42000 42001 7777 8060 ];
+    };
+  };
 
   # Set your time zone.
   time.timeZone = "Europe/Moscow";
 
-  # Select internationalisation properties.
-  i18n.defaultLocale = "en_US.UTF-8";
-  i18n.extraLocaleSettings = {
-    LC_ADDRESS = "ru_RU.UTF-8";
-    LC_IDENTIFICATION = "ru_RU.UTF-8";
-    LC_MEASUREMENT = "ru_RU.UTF-8";
-    LC_MONETARY = "ru_RU.UTF-8";
-    LC_NAME = "ru_RU.UTF-8";
-    LC_NUMERIC = "ru_RU.UTF-8";
-    LC_PAPER = "ru_RU.UTF-8";
-    LC_TELEPHONE = "ru_RU.UTF-8";
-    LC_TIME = "en_GB.UTF-8";
-  };
-
-  services.xserver.enable = true;
-
-  services.xserver.xkb = {
-    layout = "us";
-    variant = "";
-  };
-
-  services.printing = {
-    enable = true;
-    drivers = [ pkgs.hplipWithPlugin ];
-  };
-
   zramSwap.enable = true;
-
   security.rtkit.enable = true;
-  services.pulseaudio.enable = false;
-  services.pipewire = {
-    enable = true;
-    alsa.enable = true;
-    alsa.support32Bit = true;
-    pulse.enable = true;
-    jack.enable = true;
+
+  security.pam.services.${extraOptions.displayManager or "gdm"}.enableGnomeKeyring = true;
+  # security.pam.services.${extraOptions.displayManager or "gdm"}.kwallet.enable = true;
+  security.polkit.enable = true;
+
+  # Select internationalisation properties.
+  i18n = {
+    defaultLocale = "en_US.UTF-8";
+    extraLocaleSettings = {
+      LC_ADDRESS = "ru_RU.UTF-8";
+      LC_IDENTIFICATION = "ru_RU.UTF-8";
+      LC_MEASUREMENT = "ru_RU.UTF-8";
+      LC_MONETARY = "ru_RU.UTF-8";
+      LC_NAME = "ru_RU.UTF-8";
+      LC_NUMERIC = "ru_RU.UTF-8";
+      LC_PAPER = "ru_RU.UTF-8";
+      LC_TELEPHONE = "ru_RU.UTF-8";
+      LC_TIME = "en_GB.UTF-8";
+    };
   };
 
-  services.libinput.enable = true;
-  services.flatpak.enable = true;
-  services.tailscale.enable = true;
-  services.atuin.enable = true;
-  services.gnome.gnome-keyring.enable = true;
+  services = {
+    xserver = {
+      enable = true;
+      wacom.enable = true;
+      xkb = {
+        layout = "us";
+        variant = "";
+      };
+      displayManager.${extraOptions.displayManager or "gdm"}.enable = true;
+    };
+    printing = {
+      enable = true;
+      drivers = [ pkgs.hplipWithPlugin ];
+    };
+    pulseaudio.enable = false;
+    pipewire = {
+      enable = true;
+      alsa.enable = true;
+      alsa.support32Bit = true;
+      pulse.enable = true;
+      jack.enable = true;
+    };
+    libinput.enable = true;
+    flatpak.enable = true;
+    tailscale.enable = true;
+    atuin.enable = true;
+    gnome.gnome-keyring.enable = true;
+  };
 
   # users.users.guest = {
   #   description = "Guest";
@@ -156,48 +166,43 @@
     # };
   };
 
-  security.pam.services.${extraOptions.displayManager or "gdm"}.enableGnomeKeyring = true;
-  # security.pam.services.${extraOptions.displayManager or "gdm"}.kwallet.enable = true;
-  security.polkit.enable = true;
-
   nixpkgs.config.allowUnfree = true;
 
-  programs.nix-ld.enable = true;
-  # programs.nix-ld.libraries = with pkgs; [
-  #   vulkan-loader
-  #   # glib
-  #   libGL
-  #   xorg.libX11
-  #   xorg.libXcursor
-  #   xorg.libXinerama
-  #   xorg.libXext
-  #   xorg.libXrandr
-  #   xorg.libXrender
-  #   xorg.libXi
-  #   xorg.libXfixes
-  #   # stdenv.cc.cc.lib
-  #   # fontconfig
-  #   # freetype
-  #   # dbus
-  #   libxkbcommon
-  #   alsa-lib
-  # ];
-
-  programs.kdeconnect = {
-    enable = true;
-    package = pkgs-unstable.valent;
-  };
-
-  programs.bash.blesh.enable = true;
-  programs.dconf.enable = true;
-  programs.gnupg = {
-    agent = {
+  programs = {
+    nix-ld.enable = true;
+    # programs.nix-ld.libraries = with pkgs; [
+    #   vulkan-loader
+    #   # glib
+    #   libGL
+    #   xorg.libX11
+    #   xorg.libXcursor
+    #   xorg.libXinerama
+    #   xorg.libXext
+    #   xorg.libXrandr
+    #   xorg.libXrender
+    #   xorg.libXi
+    #   xorg.libXfixes
+    #   # stdenv.cc.cc.lib
+    #   # fontconfig
+    #   # freetype
+    #   # dbus
+    #   libxkbcommon
+    #   alsa-lib
+    # ];
+    kdeconnect = {
       enable = true;
-      pinentryPackage = pkgs.pinentry-gnome3;
+      package = pkgs-unstable.valent;
     };
+    bash.blesh.enable = true;
+    dconf.enable = true;
+    gnupg = {
+      agent = {
+        enable = true;
+        pinentryPackage = pkgs.pinentry-gnome3;
+      };
+    };
+    seahorse.enable = true;
   };
-  programs.seahorse.enable = true;
-
   environment.systemPackages = with pkgs-unstable; [
     # (appimage-run.override {
     #   extraPkgs = pkgs: [
@@ -366,12 +371,6 @@
       ];
     })
   ];
-
-  networking.firewall = {
-    checkReversePath = false;
-    allowedTCPPorts = [ 42000 42001 7777 8060 ];
-    allowedUDPPorts = [ 42000 42001 7777 8060 ];
-  };
 
   system.stateVersion = extraOptions.nixVersion;
 }
