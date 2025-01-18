cbb0b4cb6a5226044e62fa8b947a51864c1328ff
BasilYes <basilyevs@gmail.com>
2025-01-18 17:09:39 +0300

    new vpn script

--- config/hypr/scripts/toggle_vpn.sh
+++ config/hypr/scripts/toggle_vpn.sh
@@ -1,8 +1,17 @@
 #!/usr/bin/env bash
 
-active=$(nmcli c show --active | grep $1)
-if [[ -z $active ]];then
-    nmcli c up $1
-else
-    nmcli c down $1
+for arg; do
+	if [[ $arg != $1 ]]; then
+    nmcli c down $arg
+	fi
+done
+
+if [[ $1 != "none" ]]; then
+	active=$(nmcli c show --active | grep $1)
+	if [[ -z $active ]];then
+			nmcli c up $1
+	else
+			nmcli c down $1
+	fi
 fi
+
